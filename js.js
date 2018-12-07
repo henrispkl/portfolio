19c122ca92c4935844ac0d6e76d05d3ac85be4f7
henrisparkle <henriqueirineu10@hotmail.com>
2018-12-07 20:45:10 -0300

    Initial commit

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ var mainInfo = document.getElementsByClassName("main-info");
 var skills = document.getElementsByClassName("skills");
 
 setTimeout(() => {
-  mainInfo[0].style.marginTop = "50px";
+  mainInfo[0].style.transform = "translate(0, 0)";
   mainInfo[0].style.opacity = "1";
   mainInfo[0].style.filter = "blur(0px)";
 }, 1000);
@@ -23,48 +23,69 @@ function isVisible(elm, dist) {
 }
 
 var projTitleElem = document.getElementsByClassName("proj-title")[0];
+var projTitleName = document.getElementsByClassName("proj-titlename")[0];
 var textSectionElem = document.getElementsByClassName("text-section");
 
 window.onscroll = function() {
+  if (isVisible(textSectionElem[0], 150)) {
+    textSectionElem[0].style.opacity = 1;
+    textSectionElem[0].classList.add("_shown");
+  }
+
+  setTimeout(() => {
+    if (isVisible(textSectionElem[1], 150)) {
+      textSectionElem[1].style.opacity = 1;
+      textSectionElem[1].classList.add("_shown");
+    }
+  }, 1000);
+
   if (isVisible(projTitleElem, 150)) {
     projTitleElem.style.opacity = 1;
     projTitleElem.style.filter = "blur(0px)";
 
     projTitleElem.classList.add("_shown");
   }
-
-  for (var i = 0; i < textSectionElem.length; i++) {
-    if (isVisible(textSectionElem[i], 150)) {
-      textSectionElem[i].style.opacity = 1;
-
-      textSectionElem[i].classList.add("_shown");
-    }
-  }
 };
 
 var sklSec = document.getElementsByClassName("skl-sec");
+var skillsTitle = document.getElementsByClassName("skills-title");
 
 for (let i = 0; i < sklSec.length; i++) {
   sklSec[i].addEventListener("mouseover", function() {
     for (var i2 = 0; i2 < sklSec.length; i2++) {
       sklSec[i2].style.opacity = "0.4";
     }
+    skillsTitle[0].style.opacity = "1";
     sklSec[i].style.transform = "scale(1.4)";
     sklSec[i].style.margin = "0 30px";
     sklSec[i].classList.add("skl-hover");
 
-    sklSec[i + 1].style.transform = "scale(1.1)";
-    sklSec[i - 1].style.transform = "scale(1.1)";
+    if (i == 0) {
+    } else {
+      sklSec[i].previousElementSibling.style.transform = "scale(1.2)";
+    }
+
+    if (i == sklSec.length - 1) {
+    } else {
+      sklSec[i].nextElementSibling.style.transform = "scale(1.2)";
+    }
   });
   sklSec[i].addEventListener("mouseout", function() {
     for (let i2 = 0; i2 < sklSec.length; i2++) {
       sklSec[i2].style.opacity = "1";
     }
+    skillsTitle[0].style.opacity = "0";
     sklSec[i].style.transform = "scale(1)";
     sklSec[i].style.margin = "0 10px";
     sklSec[i].classList.remove("skl-hover");
 
-    sklSec[i + 1].style.transform = "scale(1)";
-    sklSec[i - 1].style.transform = "scale(1)";
+    if (i == 0) {
+    } else {
+      sklSec[i].previousElementSibling.style.transform = "scale(1)";
+    }
+    if (i == sklSec.length - 1) {
+    } else {
+      sklSec[i].nextElementSibling.style.transform = "scale(1)";
+    }
   });
 }
