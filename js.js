6d5b8d7e48c1c8e576bd408016943021abf72a43
henrisparkle <henriqueirineu10@hotmail.com>
2018-12-12 21:38:06 -0300

    Initial commit

--- js.js
+++ js.js
@@ -1,3 +1,9 @@
+// Device width
+var dvWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
+
+if (dvWidth > 320 && dvWidth < 480) {
+}
+
 var mainInfo = document.getElementsByClassName("main-info");
 var skills = document.getElementsByClassName("skills");
 var mLinks = document.getElementsByClassName("media-links");
@@ -28,12 +34,91 @@ function isVisible(elm, dist) {
   }
 }
 
+// Skills hover effect desktop
+var sklSec = document.getElementsByClassName("skl-sec");
+var skillsTitle = document.getElementsByClassName("skills-title");
+
+if (dvWidth > 1024) {
+  // Hover in skills
+  for (let i = 0; i < sklSec.length; i++) {
+    sklSec[i].addEventListener("mouseover", function() {
+      for (var i2 = 0; i2 < sklSec.length; i2++) {
+        sklSec[i2].style.opacity = "0.4";
+      }
+      skillsTitle[0].style.opacity = "1";
+      sklSec[i].style.transform = "scale(1.4)";
+      sklSec[i].style.margin = "0 30px";
+      sklSec[i].classList.add("skl-hover");
+
+      if (i == 0) {
+      } else {
+        sklSec[i].previousElementSibling.style.transform = "scale(1.2)";
+      }
+
+      if (i == sklSec.length - 1) {
+      } else {
+        sklSec[i].nextElementSibling.style.transform = "scale(1.2)";
+      }
+    });
+    sklSec[i].addEventListener("mouseout", function() {
+      for (let i2 = 0; i2 < sklSec.length; i2++) {
+        sklSec[i2].style.opacity = "1";
+      }
+      skillsTitle[0].style.opacity = "0";
+      sklSec[i].style.transform = "scale(1)";
+      sklSec[i].style.margin = "0 10px";
+      sklSec[i].classList.remove("skl-hover");
+
+      if (i == 0) {
+      } else {
+        sklSec[i].previousElementSibling.style.transform = "scale(1)";
+      }
+      if (i == sklSec.length - 1) {
+      } else {
+        sklSec[i].nextElementSibling.style.transform = "scale(1)";
+      }
+    });
+  }
+} else {
+  // Skills for mobile
+  console.log("skills in portable");
+  // Skills in portable
+  var showSkl_i = 0;
+  function showSkl() {
+    setTimeout(function() {
+      // Show skills title
+      document.getElementsByClassName("skills-title")[0].style.opacity = "1";
+      sklSec[showSkl_i].style.opacity = "1";
+      showSkl_i++;
+      if (showSkl_i < sklSec.length) {
+        showSkl();
+      }
+    }, 200);
+  }
+
+  // Trigger showSkl() only once
+  var _shownSkl = false;
+
+  function executeShowSkl() {
+    console.log("scroll");
+    if (isVisible(document.getElementsByClassName("skills")[0], 100)) {
+      if (_shownSkl == false) {
+        showSkl();
+      }
+
+      _shownSkl = true;
+    }
+  }
+
+  window.addEventListener("scroll", executeShowSkl);
+}
+
 var projTitleElem = document.getElementsByClassName("proj-title")[0];
 var projTitleName = document.getElementsByClassName("proj-titlename")[0];
 var textSectionElem = document.getElementsByClassName("text-section");
 var projects = document.getElementsByClassName("project");
 
-window.onscroll = function() {
+function showElements() {
   if (isVisible(textSectionElem[0], 150)) {
     textSectionElem[0].style.opacity = 1;
     textSectionElem[0].classList.add("_shown");
@@ -67,47 +152,6 @@ window.onscroll = function() {
       showProject();
     }, 500);
   }
-};
-
-var sklSec = document.getElementsByClassName("skl-sec");
-var skillsTitle = document.getElementsByClassName("skills-title");
-
-for (let i = 0; i < sklSec.length; i++) {
-  sklSec[i].addEventListener("mouseover", function() {
-    for (var i2 = 0; i2 < sklSec.length; i2++) {
-      sklSec[i2].style.opacity = "0.4";
-    }
-    skillsTitle[0].style.opacity = "1";
-    sklSec[i].style.transform = "scale(1.4)";
-    sklSec[i].style.margin = "0 30px";
-    sklSec[i].classList.add("skl-hover");
-
-    if (i == 0) {
-    } else {
-      sklSec[i].previousElementSibling.style.transform = "scale(1.2)";
-    }
-
-    if (i == sklSec.length - 1) {
-    } else {
-      sklSec[i].nextElementSibling.style.transform = "scale(1.2)";
-    }
-  });
-  sklSec[i].addEventListener("mouseout", function() {
-    for (let i2 = 0; i2 < sklSec.length; i2++) {
-      sklSec[i2].style.opacity = "1";
-    }
-    skillsTitle[0].style.opacity = "0";
-    sklSec[i].style.transform = "scale(1)";
-    sklSec[i].style.margin = "0 10px";
-    sklSec[i].classList.remove("skl-hover");
-
-    if (i == 0) {
-    } else {
-      sklSec[i].previousElementSibling.style.transform = "scale(1)";
-    }
-    if (i == sklSec.length - 1) {
-    } else {
-      sklSec[i].nextElementSibling.style.transform = "scale(1)";
-    }
-  });
 }
+
+window.addEventListener("scroll", showElements);
